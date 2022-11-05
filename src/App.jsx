350a5a3394a5d4070a8b313b9ef316f485fab6b2
Phi Truong <nguyenairport56@gmail.com>
2022-11-05 23:07:47 +0700

    save to file and copy feature

--- src/App.jsx
+++ src/App.jsx
@@ -11,14 +11,25 @@ function App() {
   }
 
   const generate = async () => {
+    if (!wordCount) return;
     const p = randomWords(wordCount).join(' ');
-    console.log(p);
     if (useROT) {
       const r = rot13(p);
       setParagraph(r);
     } else {
       setParagraph(p);
     }
+  }
+
+  const copyToClipboard = () => {
+    if (paragraph === '') return;
+    alert("Data copied!!");
+    navigator.clipboard.writeText(paragraph);
+  }
+
+  const saveToFile = () => {
+    if (paragraph === '') return;
+    window.open("data:application/txt," + encodeURIComponent(paragraph), "_self");
   }
 
   return (
@@ -29,6 +40,11 @@ function App() {
           id="word-count"
           type="number"
           onChange={e => setWordCount(e.target.valueAsNumber)}
+          onKeyDown={e => {
+            if (e.key === 'Enter') {
+              generate();
+            }
+          }}
           placeholder="Number of words..."
           className="border-stone-900 border-2 rounded-lg px-2 py-1"
         />
@@ -36,20 +52,31 @@ function App() {
           <input id="rot_checkbox" type="checkbox" checked={useROT} onChange={e => setUseROT(!useROT)} />
           <label
             htmlFor="rot_checkbox"
-            className="select-none"
+            className="select-none mb-3"
           >Use ROT13 Encryption</label>
         </div>
         <button
           onClick={() => generate()}
-          className="mt-2 px-4 py-2 bg-green-500 hover:bg-green-700 rounded-lg text-white font-bold"
+          className="mx-5 mt-2 px-4 py-2 bg-green-500 hover:bg-green-700 rounded-lg text-white font-bold"
         >
           Generate
+        </button>
+        <button
+          onClick={() => saveToFile()}
+          className="mx-5 mt-2 px-4 py-2 bg-blue-500 hover:bg-blue-700 rounded-lg text-white font-bold"
+        >
+          Save
+        </button>
+        <button
+          onClick={() => copyToClipboard()}
+          className="mx-5 mt-2 px-4 py-2 bg-yellow-500 hover:bg-yellow-700 rounded-lg text-white font-bold"
+        >
+          Copy
         </button>
         <hr className="my-4" />
       </div>
       <div
-        id="paragraph"
-        className="px-5"
+        className="px-5" id="paragraph"
       >
         <p className="text-justify">
           {paragraph}
