a35581ff2eecd8fca6e7cb7f6c4bba5819bec752
Phi Truong <nguyenairport56@gmail.com>
2022-11-14 02:06:30 +0700

    copy function now works

--- src/App.jsx
+++ src/App.jsx
@@ -21,10 +21,28 @@ function App() {
     }
   }
 
-  const copyToClipboard = async () => {
+  function copyToClipboard() {
     if (paragraph === '') return;
-    await navigator.clipboard.writeText(paragraph);
-    alert("Data copied!");
+    let target = document.getElementById("paragraph");
+    navigator.clipboard.writeText(target.innerText).then(() => {
+      document.getElementById("copied-ok").innerText = "Section copied!"
+      document.getElementById("copied-ok").style.display = "block";
+      setTimeout(() => {
+      document.getElementById("copied-ok").innerText = ""
+      document.getElementById("copied-ok").style.display = "none";
+      }, 3000);
+    }).catch(() => {
+      document.getElementById("copied-error").innerText = "Error when copy"
+      document.getElementById("copied-error").style.display = "block";
+      setTimeout(() => {
+      document.getElementById("copied-error").innerText = ""
+      document.getElementById("copied-error").style.display = "none";
+      }, 3000);
+    })
+
+    if (1) {
+      
+    }
   }
 
   const saveToFile = () => {
@@ -56,29 +74,33 @@ function App() {
           >Use ROT13 Encryption</label>
         </div>
         <button
-          onClick={() => generate()}
+          onClick={generate}
           className="mx-5 mt-2 px-4 py-2 bg-green-500 hover:bg-green-700 rounded-lg text-white font-bold select-none"
         >
           Generate
         </button>
         <button
-          onClick={() => saveToFile()}
+          onClick={saveToFile}
           className="mx-5 mt-2 px-4 py-2 bg-blue-500 hover:bg-blue-700 rounded-lg text-white font-bold select-none"
         >
           Save
         </button>
         <button
-          onClick={() => copyToClipboard()}
+          onClick={copyToClipboard}
           className="mx-5 mt-2 px-4 py-2 bg-yellow-500 hover:bg-yellow-700 rounded-lg text-white font-bold select-none"
         >
           Copy
         </button>
+        <h1 id="copied-ok" className="text-center text-green-500 font-bold mt-4" style={{display: "none"}}>
+        </h1>
+        <h1 id="copied-error" className="text-center text-red-500 font-bold mt-4" style={{display: "none"}}>
+        </h1>
         <hr className="my-4" />
       </div>
       <div
-        className="px-5" id="paragraph"
+        className="px-5"
       >
-        <p className="text-justify">
+        <p className="text-justify" id="paragraph">
           {paragraph}
         </p>
       </div>
